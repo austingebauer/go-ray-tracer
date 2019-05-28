9936d7e76f4d5d34d87aeca43595bbd131860790
austingebauer <gebauer.austin@gmail.com>
2019-05-28 16:49:44 -0700

    docs: update function comment for Float64Equals

--- utils/math.go
+++ utils/math.go
@@ -6,7 +6,7 @@ const (
 	Epsilon float64 = 0.00001
 )
 
-// Float64Equals returns two if the passed float64 values are equal.
+// Float64Equals returns true if the passed float64 values are equal.
 // The two passed float64 values are considered equal if the absolute
 // values of their difference is less than the passed epsilon.
 func Float64Equals(a, b, epsilon float64) bool {
