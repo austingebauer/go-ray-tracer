e65e4e051212139ac067b691a4203520dfd6345a
austingebauer <gebauer.austin@gmail.com>
2019-06-25 20:24:28 -0700

    feat: add Matrix equals method and unit tests

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,13 +1,42 @@
 package matrix
 
-// Matrix is a two-dimensional slice of floating point numbers.
-type Matrix [][]float64
+import "github.com/austingebauer/go-ray-tracer/utils"
 
-// NewMatrix returns a new two-dimensional matrix having the passed rows and columns.
-func NewMatrix(rows, cols uint) Matrix {
-	m := make([][]float64, rows)
-	for row := range m {
-		m[row] = make([]float64, cols)
+// Matrix represents an n-dimensional grid of floating point numbers.
+type Matrix struct {
+	rows, cols uint
+	data       [][]float64
+}
+
+// NewMatrix returns a new Matrix having the passed row and column lengths.
+func NewMatrix(rows, cols uint) *Matrix {
+	m := Matrix{
+		rows: rows,
+		cols: cols,
 	}
-	return m
+
+	d := make([][]float64, m.rows)
+	for row := range d {
+		d[row] = make([]float64, m.cols)
+	}
+	m.data = d
+
+	return &m
+}
+
+// Equals returns true if this Matrix has identical rows, columns, and elements as the passed Matrix.
+func (m *Matrix) Equals(mQ *Matrix) bool {
+	if m.rows != mQ.rows || m.cols != mQ.cols {
+		return false
+	}
+
+	for r := 0; r < int(m.rows); r++ {
+		for c := 0; c < int(m.cols); c++ {
+			if !utils.Float64Equals(m.data[r][c], mQ.data[r][c], utils.Epsilon) {
+				return false
+			}
+		}
+	}
+
+	return true
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -14,56 +14,158 @@ func TestNewMatrix(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want Matrix
+		want *Matrix
 	}{
 		{
-			name: "create 4x4 matrix",
-			args: args{
-				rows: 4,
-				cols: 4,
-			},
-			want: [][]float64{
-				{0.0, 0.0, 0.0, 0.0},
-				{0.0, 0.0, 0.0, 0.0},
-				{0.0, 0.0, 0.0, 0.0},
-				{0.0, 0.0, 0.0, 0.0},
-			},
-		},
-		{
-			name: "create 2x2 matrix",
+			name: "create new 2x2 matrix",
 			args: args{
 				rows: 2,
 				cols: 2,
 			},
-			want: [][]float64{
-				{0.0, 0.0},
-				{0.0, 0.0},
+			want: &Matrix{
+				rows: 2,
+				cols: 2,
+				data: [][]float64{
+					{0.0, 0.0},
+					{0.0, 0.0},
+				},
 			},
 		},
 		{
-			name: "create 3x1 matrix",
-			args: args{
-				rows: 3,
-				cols: 1,
-			},
-			want: [][]float64{
-				{0.0},
-				{0.0},
-				{0.0},
-			},
-		},
-		{
-			name: "create 0x0 matrix",
+			name: "create new 0x0 matrix",
 			args: args{
 				rows: 0,
 				cols: 0,
 			},
-			want: [][]float64{},
+			want: &Matrix{
+				rows: 0,
+				cols: 0,
+				data: [][]float64{},
+			},
+		},
+		{
+			name: "create new 1x0 matrix",
+			args: args{
+				rows: 1,
+				cols: 0,
+			},
+			want: &Matrix{
+				rows: 1,
+				cols: 0,
+				data: [][]float64{
+					{},
+				},
+			},
+		},
+		{
+			name: "create new 0x1 matrix",
+			args: args{
+				rows: 0,
+				cols: 1,
+			},
+			want: &Matrix{
+				rows: 0,
+				cols: 1,
+				data: [][]float64{},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, NewMatrix(tt.args.rows, tt.args.cols))
+		})
+	}
+}
+
+func TestMatrix_Equals(t *testing.T) {
+	type fields struct {
+		rows uint
+		cols uint
+		data [][]float64
+	}
+	type args struct {
+		mQ *Matrix
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "2x2 matrix equals 2x2 matrix",
+			fields: fields{
+				rows: 2,
+				cols: 2,
+				data: [][]float64{
+					{1.0, 2.0},
+					{3.0, -4.0},
+				},
+			},
+			args: args{
+				mQ: &Matrix{
+					rows: 2,
+					cols: 2,
+					data: [][]float64{
+						{1.0, 2.0},
+						{3.0, -4.0},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "2x2 matrix does not equal 2x2 matrix",
+			fields: fields{
+				rows: 2,
+				cols: 2,
+				data: [][]float64{
+					{1.0, 2.0},
+					{3.0, -4.0},
+				},
+			},
+			args: args{
+				mQ: &Matrix{
+					rows: 2,
+					cols: 2,
+					data: [][]float64{
+						{-1.0, 2.0},
+						{3.0, 4.0},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "2x3 matrix does not equal 1x1 matrix",
+			fields: fields{
+				rows: 2,
+				cols: 3,
+				data: [][]float64{
+					{1.0, 2.0, 3.0},
+					{3.0, -4.0, -1.3},
+				},
+			},
+			args: args{
+				mQ: &Matrix{
+					rows: 1,
+					cols: 1,
+					data: [][]float64{
+						{-1.0},
+					},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Matrix{
+				rows: tt.fields.rows,
+				cols: tt.fields.cols,
+				data: tt.fields.data,
+			}
+			assert.Equal(t, tt.want, m.Equals(tt.args.mQ))
 		})
 	}
 }
