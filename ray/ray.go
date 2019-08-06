973430c015df6c247ee02445f21e2fe3058d1005
austingebauer <gebauer.austin@gmail.com>
2019-08-05 19:03:22 -0700

    feat: adds NewRay function to constuct new Rays

--- ray/ray.go
+++ ray/ray.go
@@ -1,2 +1,21 @@
-// Package ray represents a ray, which has an origin and a direction.
+// Package ray represents a ray, or line, which has an origin and direction.
 package ray
+
+import (
+	"github.com/austingebauer/go-ray-tracer/point"
+	"github.com/austingebauer/go-ray-tracer/vector"
+)
+
+// Ray is a ray, or line, which has an origin and direction.
+type Ray struct {
+	Origin    *point.Point
+	Direction *vector.Vector
+}
+
+// NewRay returns a new Ray having the passed origin and direction.
+func NewRay(origin *point.Point, direction *vector.Vector) *Ray {
+	return &Ray{
+		Origin:    origin,
+		Direction: direction,
+	}
+}
--- ray/ray_test.go
+++ ray/ray_test.go
@@ -1,1 +1,36 @@
 package ray
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/austingebauer/go-ray-tracer/point"
+	"github.com/austingebauer/go-ray-tracer/vector"
+)
+
+func TestNewRay(t *testing.T) {
+	type args struct {
+		origin    *point.Point
+		direction *vector.Vector
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "ray has origin and direction",
+			args: args{
+				origin:    point.NewPoint(1, 2, 3),
+				direction: vector.NewVector(4, 5, 6),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewRay(tt.args.origin, tt.args.direction)
+			assert.Equal(t, tt.args.origin, r.Origin)
+			assert.Equal(t, tt.args.direction, r.Direction)
+		})
+	}
+}
